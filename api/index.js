367de527f7588088ef97debff947a45b5afc9466
Muhammed Rishad AJ <rishu.rishad2019@gmail.com>
2026-08-26 11:52:57 +0530

    Delete api/index.js

--- api/index.js
+++
@@ -1,3 +0,0 @@
-// Vercel serves this file at the route "/api" (the client posts to "/api").
-// It re-exports the handler from app.js (which also remains reachable at "/api/app").
-export { default } from "./app.js";
